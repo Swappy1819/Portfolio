d729e00144a97ea3578243c77d1e3fc8301d331b
Swapnil Dhanke <Swappydhanke@gmail.com>
2022-01-12 21:54:50 +0530

    added delay to explore button

--- public/scripts/portfolio.js
+++ public/scripts/portfolio.js
@@ -1,10 +1,13 @@
 
 const rotationPlate = document.querySelector('.rotation-plate');
 
-
 const explore = document.getElementById('explore-button');
 explore.addEventListener('click', function () {
     // const raje = document.getElementById('name');
     // raje.style.color = "yellow";
     rotationPlate.classList.replace("rotation-plate", "rotation-after");
 })
+
+function toMain() {
+    window.location.href="#"
+  }
